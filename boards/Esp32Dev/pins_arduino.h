1d0e0e34d351be62ff19d963434f0c19e3b49ac0
Taxi-TH <0630789001a@gmail.com>
2026-09-08 07:47:19 +0700

    Update pins_arduino.h

--- boards/Esp32Dev/pins_arduino.h
+++ boards/Esp32Dev/pins_arduino.h
@@ -4,17 +4,23 @@
 #include "soc/soc_caps.h"
 #include <stdint.h>
 
-static const uint8_t TX = 1;
-static const uint8_t RX = 3;
-static const uint8_t TXD2 = 17;
-static const uint8_t RXD2 = 16;
-static const uint8_t SDA = 21;
-static const uint8_t SCL = 22;
+#define USB_VID 0x303a
+#define USB_PID 0x1001
 
-static const uint8_t SS = 5;
-static const uint8_t MOSI = 23;
-static const uint8_t MISO = 19;
-static const uint8_t SCK = 18;
+static const uint8_t TX = 43;
+static const uint8_t RX = 44;
+
+static const uint8_t TXD2 = 1;
+static const uint8_t RXD2 = 2;
+
+static const uint8_t SDA = 13;
+static const uint8_t SCL = 15;
+
+// Modified elsewhere
+static const uint8_t SS = -1;
+static const uint8_t MOSI = -1;
+static const uint8_t MISO = -1;
+static const uint8_t SCK = -1;
 
 static const uint8_t G0 = 0;
 static const uint8_t G1 = 1;
@@ -22,36 +28,44 @@ static const uint8_t G2 = 2;
 static const uint8_t G3 = 3;
 static const uint8_t G4 = 4;
 static const uint8_t G5 = 5;
+static const uint8_t G6 = 6;
+static const uint8_t G7 = 7;
+static const uint8_t G8 = 8;
+static const uint8_t G9 = 9;
+static const uint8_t G10 = 10;
+static const uint8_t G11 = 11;
 static const uint8_t G12 = 12;
 static const uint8_t G13 = 13;
 static const uint8_t G14 = 14;
 static const uint8_t G15 = 15;
-static const uint8_t G22 = 22;
-static const uint8_t G27 = 27;
-static const uint8_t G32 = 32;
-static const uint8_t G33 = 33;
-static const uint8_t G34 = 34;
-static const uint8_t G35 = 35;
-static const uint8_t G36 = 36;
 static const uint8_t G39 = 39;
+static const uint8_t G40 = 40;
+static const uint8_t G41 = 41;
+static const uint8_t G42 = 42;
+static const uint8_t G43 = 43;
+static const uint8_t G44 = 44;
+static const uint8_t G46 = 46;
 
-#define RGB_LED -1
+static const uint8_t ADC1 = 7;
+static const uint8_t ADC2 = 8;
+
+#define RGB_LED 21
+
 #define BAD_TX 21
 #define BAD_RX 22
 
+// SERIAL (GPS) dedicated pins
 #define SERIAL_TX 21
 #define SERIAL_RX 22
 #define GPS_SERIAL_TX SERIAL_TX
 #define GPS_SERIAL_RX SERIAL_RX
-
-// ตั้งค่าปุ่มกด 5 ปุ่มแยกอิสระ (Digital 5 Buttons)
 #define HAS_BTN 1
-#define HAS_5_BUTTONS 1
 #define SEL_BTN 34
 #define UP_BTN 36
 #define DW_BTN 35
 #define R_BTN 39
 #define L_BTN 13
+#define HAS_5_BUTTONS
 #define BTN_ALIAS "\"Ok\""
 #define BTN_ACT LOW
 
@@ -59,24 +73,14 @@ static const uint8_t G39 = 39;
 #define LED_ON HIGH
 #define LED_OFF LOW
 
-#define SPI_SCK_PIN 18
-#define SPI_MISO_PIN 19
-#define SPI_MOSI_PIN 23
-#define SPI_SS_PIN 5
-
-#define IR_TX_PINS '{{"IR TX", 13}}'
-#define IR_RX_PINS '{{"IR RX", 14}}'
-#define IR_TX_PIN 13
-#define IR_RX_PIN 14
-
-#define CC1101_GDO0_PIN 22
-#define CC1101_SS_PIN 27
+#define CC1101_GDO0_PIN -1
+#define CC1101_SS_PIN -1
 #define CC1101_MOSI_PIN SPI_MOSI_PIN
 #define CC1101_SCK_PIN SPI_SCK_PIN
 #define CC1101_MISO_PIN SPI_MISO_PIN
 
-#define NRF24_CE_PIN 16
-#define NRF24_SS_PIN 17
+#define NRF24_CE_PIN -1
+#define NRF24_SS_PIN -1
 #define NRF24_MOSI_PIN SPI_MOSI_PIN
 #define NRF24_SCK_PIN SPI_SCK_PIN
 #define NRF24_MISO_PIN SPI_MISO_PIN
@@ -88,30 +92,18 @@ static const uint8_t G39 = 39;
 #define HAS_SCREEN 1
 #define ROTATION 0
 #define MINBRIGHT 160
-#define USER_SETUP_LOADED 1
-#define ST7789_DRIVER 1
-#define TFT_RGB_ORDER 1
-#define TFT_WIDTH 172
-#define TFT_HEIGHT 320
-#define TFT_MOSI 23
-#define TFT_SCLK 18
-#define TFT_CS   15
-#define TFT_DC   2
-#define TFT_RST  4
-#define TFT_BL   32 
-#define TFT_BACKLIGHT_ON 1
-#define TOUCH_CS -1 
 
-#undef SDCARD_CS
 #define SDCARD_CS 4
 #define SDCARD_SCK 18
 #define SDCARD_MISO 19
 #define SDCARD_MOSI 23
 
-#undef GROVE_SDA
 #define GROVE_SDA 33
-
-#undef GROVE_SCL
 #define GROVE_SCL 26
+
+#define SPI_SCK_PIN 18
+#define SPI_MISO_PIN 19
+#define SPI_MOSI_PIN 23
+#define SPI_SS_PIN 1
 
 #endif /* Pins_Arduino_h */
